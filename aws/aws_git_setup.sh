af6185b516bdec83618d6a272feab8e873309052
leerazo <flrazo@gmail.com>
2026-09-07 10:06:11 +0200

    aws tools

---
+++ aws/aws_git_setup.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install vim -y
+sudo apt-get install git -y
+sudo apt install gh -y
+
+# Some handy commands
+git config --global user.name "leerazo"
+# git auth login
+
+# Use this if there is a file you want to edit freely but do not want to push the changes back to the repo (like credentials for a Neo4j instance)
+# git update-index --assume-unchanged <filename>
